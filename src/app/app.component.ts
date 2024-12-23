caf1e36e7f2eff56eb3967ea7e54eea46460b38a
crsmilitaru97 <cristimilitaru97@gmail.com>
2024-12-23 20:32:13 +0200

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,130 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: false
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WishList';
+  name: string = '';
+  nameDialogVisible: boolean = false;
+  items: any = [];
+  displayDialog: boolean = false;
+  newItemName: string = '';
+  db!: firebase.database.Database;
+  statusOptions = [
+    { label: 'Nou', value: 'New' },
+    { label: 'Păstrat', value: 'Pending' },
+    { label: 'Luat', value: 'Done' }
+  ];
+
+  ngOnInit() {
+    if (localStorage.getItem('name')) {
+      this.name = localStorage.getItem('name') ?? '';
+    } else {
+      this.nameDialogVisible = true;
+    }
+    this.initializeFirebase();
+    this.loadItems();
+  }
+
+  initializeFirebase() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCCnHZNEzSqcL59wbEtg8ABYz9mVja2_us",
+      authDomain: "wishlist-e829a.firebaseapp.com",
+      databaseURL: "https://wishlist-e829a-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "wishlist-e829a",
+      storageBucket: "wishlist-e829a.firebasestorage.app",
+      messagingSenderId: "533375747230",
+      appId: "1:533375747230:web:fed6fd3d02a7a39e12e78f"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    this.db = firebase.database();
+  }
+
+  loadItems() {
+    const itemsRef = this.db.ref('items');
+
+    itemsRef.once('value')
+      .then((snapshot: any) => {
+        const items = snapshot.val();
+        if (items) {
+          this.items = Object.keys(items).map(key => ({
+            id: key,
+            name: items[key].name,
+            user: items[key].user,
+            statusBy: items[key].statusBy,
+            status: items[key].user == this.name ? '-' : items[key].status,
+          }));
+        } else {
+          this.items = [];
+        }
+      })
+      .catch((error: any) => {
+        console.error('Error loading items from Firebase: ', error);
+      });
+  }
+
+  showDialog() {
+    this.displayDialog = true;
+    this.newItemName = '';
+  }
+
+  updateStatus(item: any) {
+    item.statusBy = this.name;
+    const itemRef = this.db.ref('items').child(item.id);
+    itemRef.update({
+      status: item.status,
+      statusBy: this.name
+    }).then(() => {
+      console.log(`Item ${item.id} status updated to: ${item.status}`);
+    }).catch((error: any) => {
+      console.error('Error updating item in Realtime Database: ', error);
+    });
+  }
+
+  addItem() {
+    if (this.newItemName.trim()) {
+      const newItem = {
+        name: this.newItemName,
+        user: this.name,
+        status: 'New'
+      };
+      this.saveItem(newItem);
+      newItem.status = '-';
+    }
+  }
+
+  deleteItem(item: any) {
+    const itemRef = this.db.ref('items/' + item.id);
+    itemRef.remove().then(() => {
+      this.items = this.items.filter((i: any) => i.id !== item.id);
+      console.log('Item deleted successfully');
+    }).catch((error: any) => {
+      console.error('Error deleting item from Firebase: ', error);
+    });
+  }
+
+  saveItem(item: any) {
+    const itemsRef = this.db.ref('items');
+    const newItemRef = itemsRef.push(item);
+    newItemRef.then(() => {
+      item.id = newItemRef.key;
+      this.displayDialog = false;
+      this.items.push(item);
+    }).catch((error: any) => {
+      console.error('Error adding item to Realtime Database: ', error);
+    });
+  }
+
+  onEnterName() {
+    localStorage.setItem('name', this.name);
+    this.nameDialogVisible = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import Lara from '@primeng/themes/lara';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { providePrimeNG } from 'primeng/config';
+import { DialogModule } from 'primeng/dialog';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +19,20 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabel,
+    DialogModule,
+    TableModule,
+    CardModule,
+    SelectModule
   ],
-  providers: [],
+  providers: [ providePrimeNG({ theme: { preset: Lara } }) ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
